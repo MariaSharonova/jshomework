0596627015372aeead5c0fd10feaaeb4ddd2a637
Sharonova <shar10@bk.ru>
2021-06-15 19:59:15 -1200

    вынесла элементы в компоненты, но почему то не работает функция поика, подскажите что надо исправить в коде при набирании текста в поле инпута отладчик выдвет предупреждение

--- script.js
+++ script.js
@@ -4,14 +4,70 @@ const API_URL =
 
 console.log(window)
 
+Vue.component('goods-list', {
+  props: ['goods'],
+  template: `
+  <div class="goods-list">
+    <goods-item v-for="good in goods" :good="good" :key="good.product_name"></goods-item>
+  </div>
+  `
+});
+
+Vue.component('goods-item', {
+  props: ['good'],
+  template: `
+    <div class="goods-item">
+      <h3>{{ good.product_name }}</h3>
+      <p>{{ good.price }}</p>
+    </div>
+  `
+});
+
+Vue.component('search-goods', {
+    props: ['searchline', 'searchgoods'],
+    template: `
+    <div>
+      <input type="text" class="goods-search" v-model="searchline">
+      </input>
+      <button class="search-button" type="button" v-on:click="searchgoods">
+      Искать
+      </button>
+    </div>
+    `
+  }
+);
+
+Vue.component('busket-list', {
+  props: ['isvisiblecart', 'bgoods'],
+  template: `
+  <div class="busket-list" v-if='isvisiblecart == true'>
+    <h3>Корзина</h3>
+    <p v-if='bgoods.length==0'>Нет данных</p>
+    <div class="busket-item" v-for="good in bgoods">
+      <h3>{{good}}</h3>
+    </div>
+  </div>
+  `,
+});
+
+Vue.component('disconnect', {
+  props: ['isconnect'],
+  template: `
+  <div class="busket-list" v-if='isconnect == false'>
+    <p>Нет соединения</p>
+  </div>
+  `,
+});
+
 const app = new Vue({
   el: "#app",
   data: {
     goods: [],
     filteredGoods: [],
     busketGoods : [],
-    searchLine: '',
+    searchLine: '11',
     isVisibleCart: false,
+    isConnect: true,
   },
 
   methods: {
@@ -22,6 +78,7 @@ const app = new Vue({
         this.goods = catalogItems;
         this.filteredGoods = catalogItems;
       } else {
+        this.isConnect = false;
         alert("Ошибка при соединении с сервером");
       }
     },
@@ -49,3 +106,4 @@ const app = new Vue({
     await this.getProducts()
   }
 });
+
